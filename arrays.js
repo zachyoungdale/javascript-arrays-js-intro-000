2e56f7733ad77a9b62e27d9cea010affc228c7ef
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-17 22:24:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+  return array
+}
