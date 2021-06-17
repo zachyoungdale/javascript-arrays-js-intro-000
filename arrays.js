13d7b6f0b1b84b0d93df5bb613977cc388b435d7
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-17 22:16:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice[1]
+  array.slice()
   return array
 }
