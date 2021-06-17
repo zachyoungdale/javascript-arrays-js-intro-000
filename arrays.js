d677fe9232c2d47b176171f3784837cd3ae15df7
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-17 21:52:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,14 @@ function addElementToBeginningOfArray(array, element) {
   return ["foo", ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return ["foo", ...array]
+  return ['foo', ...array]
 }
 function addElementToEndOfArray(array, element) {
   return [...array, "foo"]
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  return [...array, 'foo']
+}
+function accessElementInArray(array, index) {
+  return array[3]
 }
