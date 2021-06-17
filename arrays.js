fbf4a001e84ad2aabc9aaf3581c1b3ef4f8de4d6
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-17 21:38:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
   array = [1]
-  array.push("foo")
+  ["foo", ...array]
 }
 function destructivelyAddElementToBeginningOfArray() {
 
